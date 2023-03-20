76f63624c1209b94a786b3588f0e70db13e42af8
KatieInnes <katharine.innes@gmail.com>
2023-03-20 10:20:21 -0700

    updated product form

--- client/src/components/ProductForm.jsx
+++ client/src/components/ProductForm.jsx
@@ -37,7 +37,7 @@ const ProductForm = (props) => {
             </div>
             <div className="mb-3">
                 <label>Price: </label> 
-                <input type="text" value={price} onChange={ (e) => setPrice(e.target.value) } />
+                <input type="number" value={price} onChange={ (e) => setPrice(e.target.value) } />
             </div>
             <div className="mb-3">
                 <label>Description: </label> 
